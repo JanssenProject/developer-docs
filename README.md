f008c1332009dcf00bbc314c13e97ec411e33d4c
moabu <47318409+moabu@users.noreply.github.com>
2024-03-07 16:50:08 +0300

    docs(javadocs): add javadocs generation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # javadocs
-A repository that holds the java docs
+
+As we do not host the javadocs on the janssen repository, we use this repository to host the javadocs of the most recent janssen official release.
+
+Please note we do not support hosting the javadocs for multiple tags, only the most recent release tag will be hosted here.
+
+Please use the following link to access the javadocs for the most recent janssen release:
+
+[https://docs.jans.io/stable/admin/reference/openapi/#javadocs](https://docs.jans.io/stable/admin/reference/openapi/#javadocs)
+
+## How to generate the javadocs for a specific janssen release tag
+
+1. Clone the janssen repository and checkout the release tag you want to generate the javadocs for
+    ```bash
+    git clone https://github.com/JanssenProject/jans.git
+    cd jans
+    git checkout <release-tag>
+    ```
+
+2. Generate the javadocs
+
+    ```bash
+    bash ./automation/docs/generate-javadocs.sh
+    ```
---
+++ automation/docs/generate-javadocs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+# The below variable represents the top level directory of the repository
+MAIN_DIRECTORY_LOCATION=$1
+OUTPUT_DIRECTORY=$2
+JVM_PROJECTS="jans-auth-server jans-orm jans-config-api jans-scim jans-core jans-fido2 agama"
+for module in $JVM_PROJECTS
+ do
+   echo "Generating javadocs for module: $module and all it's sub-modules"
+   mvn -f "$MAIN_DIRECTORY_LOCATION"/"$module"/pom.xml javadoc:javadoc
+
+   echo "Move generated javadocs to respective doc site location"
+
+   echo "getting locations where javadocs got generated"
+   mapfile -t generated_doc_paths < <(find "$MAIN_DIRECTORY_LOCATION/$module" -type d  -path '*/target/site/apidocs' | sed 's/\/target\/site\/apidocs//')
+
+   echo "move javadocs from each location to respective documentation site location"
+   for source_path in "${generated_doc_paths[@]}"
+   do
+     # check if the directory `$source_path` exists, if not then create one
+     mkdir -p "$source_path" || echo "Directory $source_path already exists"
+     echo "copy javadocs for $source_path"
+     cp -rv ./"$source_path"/target/site/apidocs/* "$OUTPUT_DIRECTORY""$source_path"/
+   done
+ done
