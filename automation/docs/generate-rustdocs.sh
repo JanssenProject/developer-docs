c02192f88a5e6c8d6c6ef71242b2424c4149f1fb
moabu <47318409+moabu@users.noreply.github.com>
2025-04-29 18:28:06 +0300

    ci: add generating rust docs

--- automation/docs/generate-rustdocs.sh
+++ automation/docs/generate-rustdocs.sh
@@ -7,5 +7,5 @@ for project in $RUST_PROJECTS
    cd "$project"
    cargo doc --no-deps -p cedarling
    echo "Move generated rust docs to respective doc site location"
-   cp -r target/doc ../../cedarling
+   cp -r target/doc /home/runner/work/developer-docs/cedarling
  done
