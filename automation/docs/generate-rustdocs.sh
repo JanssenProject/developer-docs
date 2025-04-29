5743bb0bba6f38582447c1f7edf9376ebe8f1dd7
moabu <47318409+moabu@users.noreply.github.com>
2025-04-29 17:48:07 +0300

    ci: add generating rust docs

---
+++ automation/docs/generate-rustdocs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+RUST_PROJECTS="jans-cedarling"
+for project in $RUST_PROJECTS
+ do
+   echo "Generating rust docs for: $project and all it's sub-modules"
+   cd "$project"
+   cargo doc --no-deps -p cedarling
+   echo "Move generated rust docs to respective doc site location"
+   cp -r target/doc ../../cedarling
+ done
