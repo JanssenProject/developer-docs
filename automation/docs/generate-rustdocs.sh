cd58e97c41df86ec7195fcc4d11bdfdf361770eb
moabu <47318409+moabu@users.noreply.github.com>
2025-05-01 15:30:29 +0300

    ci: fix location paths

--- automation/docs/generate-rustdocs.sh
+++ automation/docs/generate-rustdocs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euo pipefail
+# The below variable represents the top level directory of the repository
+MAIN_DIRECTORY_LOCATION=$1
+OUTPUT_DIRECTORY=$2
 RUST_PROJECTS="jans-cedarling"
 for project in $RUST_PROJECTS
  do
    echo "Generating rust docs for: $project and all it's sub-modules"
    cd "$project"
    cargo doc --no-deps -p cedarling
-   cp -rv target/doc ../../cedarling
+   cp -rv target/doc ../../../cedarling
  done
+
