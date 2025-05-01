6906efc94e5d7984b47ba0c4c4e3825dd64bee61
moabu <47318409+moabu@users.noreply.github.com>
2025-05-01 21:45:22 +0300

    ci: install cedarling python

--- automation/docs/generate-pythondocs.sh
+++ automation/docs/generate-pythondocs.sh
@@ -6,6 +6,4 @@ for project in $PYTHON_PROJECTS
    echo "Generating python docs for: $project and all it's sub-modules"
    cd "$project"
    python -m pydoc -w cedarling_python
-   mkdir -p ../../../../cedarling-python || echo "Directory cedarling-python already exists"
-   cp -rv cedarling_python.html ../../../../cedarling-python/cedarling_python.html
  done
