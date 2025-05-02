694157684ce31041eea9d81f44a6eb9c829f56ed
moabu <47318409+moabu@users.noreply.github.com>
2025-05-02 15:41:53 +0300

    ci: generate python doc in workflow

--- automation/docs/generate-pythondocs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-PYTHON_PROJECTS="jans-cedarling/bindings/cedarling_python"
-for project in $PYTHON_PROJECTS
- do
-   echo "Generating python docs for: $project and all it's sub-modules"
-   cd "$project"
-   python -m pydoc -w cedarling_python
- done
