8584ac3119a0fecba8fa49613957f7cd8956f6d0
moauto <54212639+mo-auto@users.noreply.github.com>
2025-05-22 23:37:28 +0000

    docs: update javadocs & rustdocs

--- cedarling/doc/trait.impl/core/convert/trait.TryFrom.js
+++ cedarling/doc/trait.impl/core/convert/trait.TryFrom.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.83.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"cedarling/enum.FeatureToggle.html\" title=\"enum cedarling::FeatureToggle\">FeatureToggle</a>"]]]]);
+    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"struct\" href=\"cedarling/struct.BootstrapConfigRaw.html\" title=\"struct cedarling::BootstrapConfigRaw\">BootstrapConfigRaw</a>&gt; for <a class=\"struct\" href=\"cedarling/struct.LockServiceConfig.html\" title=\"struct cedarling::LockServiceConfig\">LockServiceConfig</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.83.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"cedarling/enum.FeatureToggle.html\" title=\"enum cedarling::FeatureToggle\">FeatureToggle</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[442]}
+//{"start":57,"fragment_lengths":[888]}
