b9235b669324bab49110d3a09f0db3a6d3809c65
moauto <54212639+mo-auto@users.noreply.github.com>
2025-09-25 23:39:03 +0000

    docs: update javadocs & rustdocs

--- cedarling/doc/trait.impl/core/marker/trait.Copy.js
+++ cedarling/doc/trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"cedarling/enum.LogLevel.html\" title=\"enum cedarling::LogLevel\">LogLevel</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"cedarling/raw_config/enum.FeatureToggle.html\" title=\"enum cedarling::raw_config::FeatureToggle\">FeatureToggle</a>"]]]]);
+    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"cedarling/enum.LogLevel.html\" title=\"enum cedarling::LogLevel\">LogLevel</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"cedarling/raw_config/enum.FeatureToggle.html\" title=\"enum cedarling::raw_config::FeatureToggle\">FeatureToggle</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
