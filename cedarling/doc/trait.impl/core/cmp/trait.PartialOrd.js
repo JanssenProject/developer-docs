858c6dcf1a382a528ed697c853d29d99ba6c9a2f
moauto <54212639+mo-auto@users.noreply.github.com>
2025-10-27 23:45:27 +0000

    docs: update javadocs & rustdocs

--- cedarling/doc/trait.impl/core/cmp/trait.PartialOrd.js
+++ cedarling/doc/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"cedarling/enum.LogLevel.html\" title=\"enum cedarling::LogLevel\">LogLevel</a>"]]]]);
+    var implementors = Object.fromEntries([["cedarling",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"cedarling/enum.LogLevel.html\" title=\"enum cedarling::LogLevel\">LogLevel</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
